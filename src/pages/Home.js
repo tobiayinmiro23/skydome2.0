c87d97c290774e7a8750dd185dd1ddb88bf572ed
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-08-26 13:23:41 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React,{useEffect,useState} from 'react'
+import axios from 'axios';
 import { Header,SliderComponent,Input,Card,Advertisement ,Footer, NoResultForSearchBar} from '../AllFilles';
 import {laptopProductForHome} from '../database/ProdBeforeAdd'
 import {generalProductsForHome} from '../database/ProdAfterAdd'
