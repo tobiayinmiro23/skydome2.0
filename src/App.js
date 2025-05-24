10ef41d243986cb05a5cf0703ba07a3e4d0d354d
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2025-05-24 13:23:33 +0100

    Update App.js
    implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import React from 'react';
-import { Home,Checkout,Cart,Payment,Shoe,Cloth,Laptop,Phone,GenderSelection,LaptopInformation,PhoneInformation,ClothShoeAndGeneralInformation,SearchResultCheckout,Login,Unavailable,User,ForgotPassword} from './AllFilles';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import Loader from './components/Loader';
+import { Home, Checkout, Cart, Payment, Shoe, Cloth, Laptop, Phone, GenderSelection, LaptopInformation, PhoneInformation, ClothShoeAndGeneralInformation, SearchResultCheckout, Login, Unavailable, User, ForgotPassword } from './AllFilles';
 function App() {
   return (
     <div className="App">
-        <Router>
-        <Routes>
-          <Route path='/' index element={<Home/>} />
-          <Route path='/signup' element={<Login/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/checkout' element={<Checkout/>} />
-          <Route path='/user' element={<User/>} />
-          <Route path='/unavailable' element={<Unavailable/>} />
-          <Route path='/forgotpassword' element={<ForgotPassword/>} />
-          <Route path='/payment' element={<Payment/>} />
-          <Route path='/gender' element={<GenderSelection/>} />
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/cloth' element={<Cloth/>} />
-          <Route path='/laptop' element={<Laptop/>} />
-          <Route path='/phone' element={<Phone/>} />
-          <Route path='/shoe' element={<Shoe/>} />
-          <Route path='/laptopcheckout/:id' element={<LaptopInformation/>} />
-          <Route path='/phonecheckout/:id' element={<PhoneInformation/>} />
-          <Route path='/shoecheckout/:id' element={<ClothShoeAndGeneralInformation/>} />
-          <Route path='/clothcheckout/:id' element={<ClothShoeAndGeneralInformation/>} />
-          <Route path='/generalcheckout/:id' element={<ClothShoeAndGeneralInformation/>} />
-          <Route path='/searchresultcheckout/:id' element={<SearchResultCheckout/>} />
-          <Route path='/*' element={<Unavailable/>} />
-        </Routes>
-    </Router>
+      <Router>
+        <Suspense fallback={<div className='pt-[14rem]'><Loader /></div>}>
+          <Routes>
+            <Route path='/' index element={<Home />} />
+            <Route path='/signup' element={<Login />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/user' element={<User />} />
+            <Route path='/unavailable' element={<Unavailable />} />
+            <Route path='/forgotpassword' element={<ForgotPassword />} />
+            <Route path='/payment' element={<Payment />} />
+            <Route path='/gender' element={<GenderSelection />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/cloth' element={<Cloth />} />
+            <Route path='/laptop' element={<Laptop />} />
+            <Route path='/phone' element={<Phone />} />
+            <Route path='/shoe' element={<Shoe />} />
+            <Route path='/laptopcheckout/:id' element={<LaptopInformation />} />
+            <Route path='/phonecheckout/:id' element={<PhoneInformation />} />
+            <Route path='/shoecheckout/:id' element={<ClothShoeAndGeneralInformation />} />
+            <Route path='/clothcheckout/:id' element={<ClothShoeAndGeneralInformation />} />
+            <Route path='/generalcheckout/:id' element={<ClothShoeAndGeneralInformation />} />
+            <Route path='/searchresultcheckout/:id' element={<SearchResultCheckout />} />
+            <Route path='/*' element={<Unavailable />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </div>
   );
 }
