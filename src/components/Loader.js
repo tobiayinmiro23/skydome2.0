4ff31993fb7b513f50ac473ed5bf5ef1e78b8e2d
Ayinmiro tobi <87906572+tobiayinmiro23@users.noreply.github.com>
2025-05-25 15:13:29 +0100

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Loader = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <div>
-        <div  className="loading w-[98.5vw] h-[100vh] pt-[0rem]">
-           <div className="loader ml-[45%] max-[700px]:ml-[42%] max-[426px]:ml-[36%]">
-            <div className="loader1  w-[1.9rem] h-[1.9rem] rounded-[50%] m-[0.25rem] bg-[#38ef7d]"></div>
-            <div className="loader2  w-[1.9rem] h-[1.9rem] rounded-[50%] m-[0.25rem] bg-[#38ef7d]"></div>
-            <div className="loader3  w-[1.9rem] h-[1.9rem] rounded-[50%] m-[0.25rem] bg-[#38ef7d]"></div>
-          </div>
+      <div className="loading w-[98.5vw] h-[100vh] pt-[0rem]">
+        <div className="loader ml-[45%] max-[700px]:ml-[42%] max-[426px]:ml-[36%]">
+          <div className="loader1  w-[1.9rem] h-[1.9rem] rounded-[50%] m-[0.25rem] bg-[#38ef7d]"></div>
+          <div className="loader2  w-[1.9rem] h-[1.9rem] rounded-[50%] m-[0.25rem] bg-[#38ef7d]"></div>
+          <div className="loader3  w-[1.9rem] h-[1.9rem] rounded-[50%] m-[0.25rem] bg-[#38ef7d]"></div>
+        </div>
       </div>
     </div>
   )
