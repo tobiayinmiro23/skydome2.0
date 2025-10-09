4078a2aec5620678b66d0b14c15f900a21c8e68e
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-10-09 14:48:51 +0100

    Update ProductInfoButton.js

--- src/components/ProductInfoButton.js
+++ src/components/ProductInfoButton.js
@@ -16,8 +16,8 @@ const ProductInfoButton = ({ id, price }) => {
     <div>
 
       <div className="btnWrapper w-[fit-content] flex  m-[auto] my-[1.5rem] max-[321px]:block max-[321px]:text-center ">
-        <div> <PopUpMessage data={id} style='font-semibold  m-[0.6rem] px-[3rem] py-[0.6rem] bg-[#38ef7d] cursor-pointer outline-none hover:bg-[#17cf5e] max-[405px]:m-[0.2rem] max-[405px]:px-[2rem]' /></div>
-        <div onClick={handleCheckout}> <button style={{ boxShadow: '0px 1.4px 2px darkgrey' }} className="w-[90%] font-semibold m-[0.6rem] px-[3rem] py-[0.6rem] bg-[#38ef7d] cursor-pointer outline-none hover:bg-[#17cf5e] max-[405px]:my-[0.2rem] max-[405px]:px-[2rem] max-[321px]:mt-[0.8rem]" >Checkout</button></div>
+        <div> <PopUpMessage data={id} style='font-semibold  m-[0.6rem] px-[3rem] py-[0.6rem] bg-[#38ef7d] cursor-pointer outline-none hover:bg-[#17cf5e] max-[405px]:m-[0.2rem] max-[405px]:px-[2rem] max-[321px]:w-[90%]' /></div>
+        <div onClick={handleCheckout}> <button style={{ boxShadow: '0px 1.4px 2px darkgrey' }} className="w-[90%] font-semibold m-[0.6rem] px-[3rem] py-[0.6rem] bg-[#38ef7d] cursor-pointer outline-none hover:bg-[#17cf5e] max-[405px]:my-[0.2rem] max-[405px]:px-[2rem] max-[321px]:mt-[0.8rem] max-[321px]:w-[90%]" >Checkout</button></div>
       </div>
     </div>
   )
