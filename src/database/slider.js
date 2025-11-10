8af2ef22346d64156326cfcb825dd2855071dcf0
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-11-10 14:10:42 +0100

    Update slider.js

--- src/database/slider.js
+++ src/database/slider.js
@@ -1,26 +1,26 @@
 export let sliderImg=[
     {
       id:1,
-      src:'/skydome images/Slide-img1.jpg',
+      src:'/skydome images/Slide-img1.webp',
     },
     {
       id:2,
-      src:'/skydome images/Slide-img2.jpg',
+      src:'/skydome images/Slide-img2.webp',
     },
     {
       id:3,
-      src:'/skydome images/Slide-img3.jpg',
+      src:'/skydome images/Slide-img3.webp',
     },
     {
       id:4,
-      src:'/skydome images/Slide-img4.jpeg',
+      src:'/skydome images/Slide-img4.webp',
     },
     {
       id:5,
-      src:'/skydome images/Slide-img5.jpeg',
+      src:'/skydome images/Slide-img5.webp',
     },
     {
       id:6,
-      src:'/skydome images/Slide-img6.jpeg',
+      src:'/skydome images/Slide-img6.webp',
     },
   ] 
