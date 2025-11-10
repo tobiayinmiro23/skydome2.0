7aedef6dfb06ce362fd08d54048310ef4277e8db
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-11-10 13:44:27 +0100

    Update NoResultForSearchBar.js

--- src/components/NoResultForSearchBar.js
+++ src/components/NoResultForSearchBar.js
@@ -16,7 +16,7 @@ const NoResultForSearchBar = () => {
             :
             <div className='w-[50%] m-[auto] max-[1150px]:w-[65%] max-[912px]:w-[75%] max-[657px]:w-[100%]'>
                 <div>
-                    <img src="/E-commerce pictures/undraw_Empty_re_opql.png" loading='eager' alt="no result" />
+                    <img src="/E-commerce pictures/undraw_Empty_re_opql.webp" loading='eager' alt="no result" />
                 </div>
                 <h1 className='text-center productinfo  font-bold text-[1.3rem] '>Ops, We could not find any result</h1>
             </div>
